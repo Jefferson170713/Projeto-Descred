28eb6776c3001c22262c1b98a033eba2eb06984c
Jefferson new pc <jefferson.almeia05@aluno.ifce.edu.br>
2025-07-10 09:20:04 -0300

    main - ajueste erro

--- r1_descred.py
+++ r1_descred.py
@@ -38,7 +38,7 @@ class R1Descred(QMainWindow):
         self.procedure_descred.create_descred_window(self.provider_descred)
         # self.procedure_package_process.create_procedures_and_package_tab(self.provider_descred)
         # self.center_clinic_process.create_center_clinic_tab(self.center_clinic)
-        # Teste Jefferson
+        # Teste mais uma vez Jefferson
 
 
 # Loop do programa em funcionamento
