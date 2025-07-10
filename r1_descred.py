fe92e6d3b087b093bfeb0eec360cde3c0a669ef8
Jefferson new pc <jefferson.almeia05@aluno.ifce.edu.br>
2025-07-10 09:24:46 -0300

    dev - teste na mesma linha

--- r1_descred.py
+++ r1_descred.py
@@ -39,6 +39,7 @@ class R1Descred(QMainWindow):
         # self.procedure_package_process.create_procedures_and_package_tab(self.provider_descred)
         # self.center_clinic_process.create_center_clinic_tab(self.center_clinic)
         # Teste mais uma vez Jefferson
+        # Teste com o GitHub Copilot
 
 # Loop do programa em funcionamento
 def main():
